2b4620ef8b9a071b4276046ca660c9fde8f8a57f
Kaic Nunes <kaic.nunes@MASCARENHAS.CORP>
2021-12-23 11:20:06 -0400

    subindo arquivos

---
+++ ApreendendoCSharp/2-CriandoVariais/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando parte 2 - Criando Variaveis");
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
