6c15a7e5b5d22b2dcf2375bd5987fa8bdc414f36
Kaic Nunes <kaic.nunes@MASCARENHAS.CORP>
2021-12-29 11:36:03 -0400

    estudo dia 29

---
+++ ApreendendoCSharp/4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 4: ");
+
+            double salario = 1200.50;
+
+            int salarioEmInteiro = (int)salario;
+            //variável 32 bits
+
+            long idade = 130000000000000;
+            //variável 64 bits
+
+
+            short quantidadeProdutos = 150;
+            //variável 16 bits
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ApreendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5: ");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+            string titulo = "ESTUDANDO CURSO " + 2020;
+            Console.WriteLine(titulo);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
